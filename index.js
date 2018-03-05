35be6fc4a2cd63c48d7d92826eb3c05ce7e7e413
Julian Johannesen <julian.johannesen@gmail.com>
2018-03-05 20:50:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-
+  newDriver = { ...driver};
+  delete newDriver[key];
+  return newDriver;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key){
   delete driver[key];
+  return driver;
 }
