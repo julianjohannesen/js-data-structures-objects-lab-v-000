1e4aa3611e250097db8937db875ff859de419ca1
Julian Johannesen <julian.johannesen@gmail.com>
2018-03-05 20:43:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+
+const driver = {
+  name: "Sam"
+};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  this[key] = value;
+  return this;
+}
+updateDriverWithKeyAndValue(driver, "address", "11 Broadway");
+console.log(driver['address']);
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+
+}
