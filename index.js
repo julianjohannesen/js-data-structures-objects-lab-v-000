93344bb1d7e7bd261c895fc1a781bc661cfa57f5
Julian Johannesen <julian.johannesen@gmail.com>
2018-03-05 20:45:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const driver = {
 };
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  this[key] = value;
-  return this;
+  const newDriver = { ...driver };
+  newDriver[key] = value;
+  return newDriver;
 }
+
 updateDriverWithKeyAndValue(driver, "address", "11 Broadway");
-console.log(driver['address']);
+console.log(newDriver['address']);
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-
+  driver[key] = value;
+  return driver;
 }
